acb29ad13cdcef74792f64c7ce2c3377836a9c66
Max Ivanov <waxivanov@gmail.com>
2021-09-20 18:01:53 +0300

    todo app

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Home Page</h2>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolorem
+      consequatur maxime ad. Vero dolorum ab vitae velit, tempora asperiores?
+    </p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
